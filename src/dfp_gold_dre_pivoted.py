ad1afc46a5cae09128267fbd9bb03fc3078eca6b
Carlos Bologna <carlos.bologna@hotmail.com>
2024-05-27 23:17:05 +0000

    firt gold layer baseline code

--- src/dfp_gold_dre_pivoted.py
+++ src/dfp_gold_dre_pivoted.py
@@ -13,16 +13,13 @@ def main():
     # Create or connect to the DuckDB database
     conn = duckdb.connect(database=db_path, read_only=False)
 
-    # Clear the destination table dropping it
-    drop_tables_from_duckdb([destination_table], conn)
-
     # Read data
     df = conn.sql(f"SELECT * FROM {source_table}")
 
     # Filter data
     df_current_values = df.filter("""
     ORDEM_EXERC = 'ÚLTIMO' and
-    PERIODO_MESES = 3
+    (PERIODO_MESES = 3 or PERIODO_MESES = 12)
     """)
 
     df_pivot = conn.sql(f"""
@@ -30,7 +27,7 @@ def main():
             PIVOT df_current_values
             ON DS_CONTA IN ('Receita de Venda de Bens e/ou Serviços')
             USING sum(VL_CONTA)
-                        GROUP BY CNPJ_CIA, DENOM_CIA, GRUPO_DFP, MOEDA, ESCALA_MOEDA, DT_REFER, DT_INI_EXERC, DT_FIM_EXERC, VERSAO, CD_CVM
+            GROUP BY CNPJ_CIA, DENOM_CIA, GRUPO_DFP, MOEDA, ESCALA_MOEDA, DT_REFER, DT_INI_EXERC, DT_FIM_EXERC, VERSAO, CD_CVM
         )
 
     """)
@@ -38,14 +35,25 @@ def main():
     df_pivot = conn.sql('SELECT *, "Receita de Venda de Bens e/ou Serviços" AS RECEITA FROM df_pivot')
 
     columns = [item for item in df_pivot.columns if item != 'Receita de Venda de Bens e/ou Serviços']
+    # Join the columns into a string separated by commas
+    columns_str = ', '.join(columns)
 
     df_next_revenue = conn.sql(f"""
-        SELECT {columns},
-            lead(RECEITA) AS RECEITA_FUTURA
+        SELECT
+            {columns_str},
+            lead(RECEITA)
                 OVER (
                     PARTITION BY CD_CVM
-                    ORDER BY )
+                    ORDER BY DT_REFER
+                ) AS RECEITA_FUTURA
+        FROM df_pivot
     """)
+
+    # Write table
+    # Clear the destination table dropping it
+    drop_tables_from_duckdb([destination_table], conn)
+    # Create the new table
+    conn.execute(f"CREATE TABLE {destination_table} AS SELECT * FROM df_next_revenue")
 
     # Close the DuckDB connection
     conn.close()
