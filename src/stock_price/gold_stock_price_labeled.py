e43206c8ae5920df400326baa8203f28beef1b4a
Carlos Bologna <carlos.bologna@hotmail.com>
2024-09-27 11:05:11 -0300

    Changing the target from Open stock price to Close stock price.

--- src/stock_price/gold_stock_price_labeled.py
+++ src/stock_price/gold_stock_price_labeled.py
@@ -19,24 +19,24 @@ DATA_SOURCE_TABLENAME = "silver_stock_prices"
 DATA_DESTINATION_TABLENAME = "gold_stock_price_labeled"
 
 def read_table(tablename, conn):
-    
+
     df = conn.sql(f"SELECT * FROM {tablename}")
 
     return df
 
 def copy_table(source_table, destination_table, conn):
     try:
-       
+
         conn.execute(f"CREATE TABLE IF NOT EXISTS {destination_table} AS SELECT * FROM {source_table}")
 
         print(f"Copying content of the table {source_table} to {destination_table}")
-        
+
     except Exception as e:
         print(f"Error copying table {source_table}: {e}")
 
 
 def main():
-    
+
     db_path = os.path.join(DATA_SOURCE_FOLDER, DATA_SOURCE_FILENAME)
 
     # Create or connect to the DuckDB database
@@ -51,10 +51,10 @@ def main():
 
     # Create a new column 'y' with values of the next record ordered by the 'Date' column
     query = """
-    SELECT 
+    SELECT
         *,
-        LEAD(Open, 1) OVER (PARTITION BY Ticker ORDER BY Date) AS y
-    FROM 
+        LEAD(Close, 1) OVER (PARTITION BY Ticker ORDER BY Date) AS y
+    FROM
         df
     """
 
