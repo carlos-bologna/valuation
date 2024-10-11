9807480c1de08a0508276688b9cded8084379871
Carlos Bologna <carlos.bologna@hotmail.com>
2024-10-11 18:36:19 +0000

    Enablig the debug in the Flask

--- src/stock_price/predict.py
+++ src/stock_price/predict.py
@@ -104,4 +104,4 @@ def run_pipeline(ticker):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
